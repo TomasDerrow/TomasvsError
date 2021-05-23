7e9b6f9d0d5c62be6931db90938b746d7092c8fc
Tomas Derrow <tomasderrow@gmail.com>
2021-05-23 16:59:46 -0400

    started work on oop

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
 import pygame
 import os
 import time
+
+#HW: make same thing as we did for player, for the enemy, make method for enemyRange and other function, (loading images, animations), work on jump
+
+class character():
+    def __init__(self):
+        self.walkCount = 0
+        self.swingCount = 0
+        self.swingTrigger = False
+        self.blockTrigger = False
+        self.swinging = False
+        self.blocking = False
+        self.jumping = False
+        self.left = False
+        self.right = True
+        self.still = True
+        self.health = 100
+        self.alive = True
+        self.jumpCount = 10
+
+class player(character):
+    def __init__(self, playerPosX, playerPosY, knightPosX, stagePosX, knight_width, knight_height):
+        #self.knightRect = knightRect
+        #might have to add method for knightrect
+        self.playerPosX = playerPosX
+        self.playerPosY = playerPosY
+        self.knightPosX = knightPosX
+        self.stagePosX = stagePosX
+        self.knight_width = knight_width
+        self.knight_height = knight_height
+
+    def def_knightRect(self):
+        self.knightRect = pygame.Rect(self.knightPosX, 0, self.knight_width, self.knight_height)
+
+
+class enemy(character):
+    def __init__(self):
+        self.at500 = False
+        #self.enemyRange = pygame.Rect(enemyKnight.x, enemyKnight.y, 500, 200)
+        #might have to add method for enemyRange
+
+player = player(37.5, 0, 37.5, 0, 75, 67)
+player.def_knightRect()
+
 #HW - draw flipped blocking and swinging animations, be able to swing while walking
 WIDTH, HEIGHT = 900, 500
 pygame.display.set_caption("Tomas vs Error")
@@ -8,34 +51,12 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 FLOOR = pygame.Rect(0, HEIGHT-50, WIDTH, 50)
 vel = 5
-global at500
-at500 = False
-global walkCount
-walkCount = 0
-global swingCount
-swingCount = 0
-global swingTrigger
-swingTrigger = False
-global blockTrigger
-blockTrigger = False
-global enemyalive
-enemyalive = True
-global playeralive
-playeralive = True
-
-global health
-health = 100
-enemyhealth = 100
-#pygame.Surface.set_colorkey(KNIGHT_IMAGE, [255,255,255])
 
 KNIGHT_WIDTH, KNIGHT_HEIGHT = 75, 67
 ENEMY_WIDTH, ENEMY_HEIGHT = 100, 100
 
-
 knight = pygame.Rect(WIDTH/2 - KNIGHT_WIDTH, 0, KNIGHT_WIDTH, KNIGHT_HEIGHT)
-global enemy
-enemy = pygame.Rect(WIDTH/2 - ENEMY_WIDTH, HEIGHT-FLOOR.height-ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT)
-
+enemyKnight = pygame.Rect(WIDTH/2 - ENEMY_WIDTH, HEIGHT-FLOOR.height-ENEMY_HEIGHT, ENEMY_WIDTH, ENEMY_HEIGHT)
 
 KNIGHT1 = pygame.image.load(os.path.join('Assets', 'Default_Character1N.png'))
 KNIGHT2 = pygame.image.load(os.path.join('Assets', 'Default_Character2.png'))
@@ -83,41 +104,21 @@ flWidth, flHeight = FLOOR1_IMAGE_SCALED.get_rect().size
 Character_Movements = [pygame.image.load(os.path.join('Assets', 'Default_Character1N.png')), pygame.image.load(os.path.join('Assets', 'Default_Character_Flipped1.png')), pygame.image.load(os.path.join('Assets', 'Default_Character2.png'))]
 
 stageWidth = bgWidth*2
-global stagePosX
-stagePosX = 0
+#player.stagePosX = 0
 
 startScrollingPosX = (WIDTH / 2)
 
 halfW = 37.5
-global knightPosX
-knightPosX = halfW
-
-global playerPosX
-playerPosX = halfW
 #playerPosY = 500
 playerVelocityX = 0
 
-global knightrect
-knightrect = pygame.Rect(knightPosX, knight.y, KNIGHT_WIDTH, KNIGHT_HEIGHT)
+#player.knightrect = pygame.Rect(player.knightPosX, knight.y, KNIGHT_WIDTH, KNIGHT_HEIGHT)
 
-global enemyRange
-enemyRange = pygame.Rect(enemy.x, enemy.y, 500, 200)
+#enemy.enemyRange = pygame.Rect(enemyKnight.x, enemyKnight.y, 500, 200)
 
 global x
 x = 0
-global JUMPING
-JUMPING = False
 
-global LEFT
-LEFT = False
-global RIGHT
-RIGHT = True
-global STILL
-STILL = True
-global Swinging
-Swinging = False
-global Blocking
-Blocking = False
 
 walkLeft = [KNIGHTfs1, KNIGHTfs2, KNIGHTfs3, KNIGHTfs4]
 walkRight = [KNIGHTs1, KNIGHTs2, KNIGHTs3, KNIGHTs4]
@@ -132,69 +133,84 @@ swingRight = [KNIGHTsws1, KNIGHTsws2, KNIGHTsws3]
         #self.walkCount = 0
 
 def handle_movement(keys_pressed, knight, playerVelocityX):
-    global JUMPING
-    global playerPosX
-    global stagePosX
-    global knightPosX
-    global LEFT
-    global RIGHT
-    global STILL
-    global middle
-    global end
-    global health
-    global knightrect
-    #global playerPosY
-
     if pygame.key.get_pressed()[pygame.K_a]:
         playerVelocityX = -5
-        LEFT = True
-        RIGHT = False
-        STILL = False
+        player.left = True
+        player.right = False
+        player.still = False
 
     elif pygame.key.get_pressed()[pygame.K_d]:
         playerVelocityX = 5
-        LEFT = False
-        RIGHT = True
-        STILL = False
+        player.left = False
+        player.right = True
+        player.still = False
         #KNIGHT_IMAGE = pygame.image.load(os.path.join('Assets', 'Default_Character.png'))
     #RIGHT
     else:
-        STILL = True
-        walkCount = 0
+        player.still = True
+        player.walkCount = 0
 
-    if pygame.key.get_pressed()[pygame.K_SPACE] and JUMPING == False:
-        JUMPING = True
-        knight.y -= 400
+    #if not(player.jumping):
+        #if pygame.key.get_pressed()[pygame.K_SPACE]:
+            #player.jumping = True
+            #player.walkCount = 0
+            #print("jump")
+            #knight.y -= 400
+    #else:
+        #print("jump2")
+        #if player.jumpCount >= -10:
+            #neg = 1
+            #if player.jumpCount < 0:
+                #neg = -1
+            #knight.y -= (player.jumpCount ** 2) * 0.5 * neg
+            #player.jumpCount -= 1
+        #else:
+            #player.jumping = False
+            #player.jumpCount = 10
+
+    if not(player.jumping):
+        if pygame.key.get_pressed()[pygame.K_SPACE]:
+            player.jumping = True
+            player.walkCount = 0
+    else:
+        print("jump2")
+        if player.jumpCount >= -10:
+            neg = 1
+            if player.jumpCount < 0:
+                neg = -1
+            player.y -= (man.jumpCount ** 2) * 0.5 * neg
+            player.jumpCount -= 1
+        else:
+            player.isJump = False
+            player.jumpCount = 10
+#1234
 
 
 
+    player.playerPosX += playerVelocityX
 
-
-    playerPosX += playerVelocityX
-
-    if playerPosX > stageWidth:
-         playerPosX = stageWidth
+    if player.playerPosX > stageWidth:
+         player.playerPosX = stageWidth
          #makes right boundary
 
-    if playerPosX < 0:
-          playerPosX = 0
+    if player.playerPosX < 0:
+          player.playerPosX = 0
           #makes left boundary
-    if playerPosX < startScrollingPosX - knight.width:
-          knightPosX = playerPosX
+    if player.playerPosX < startScrollingPosX - knight.width:
+          player.knightPosX = player.playerPosX
          # print("here1")
           #if the screen doesn't need to scroll then the knight can move freely
-    elif playerPosX > stageWidth - startScrollingPosX:
-          knightPosX = playerPosX - stageWidth + WIDTH - knight.width
+    elif player.playerPosX > stageWidth - startScrollingPosX:
+          player.knightPosX = player.playerPosX - stageWidth + WIDTH - knight.width
           #playerPosX = stageWidth + WIDTH
           #print("here12")
           #right boundary
     else:
           #playerPosX = startScrollingPosX - knight.width
-          knightPosX = startScrollingPosX - knight.width
-          stagePosX += playerVelocityX
+          player.knightPosX = startScrollingPosX - knight.width
+          player.stagePosX += playerVelocityX
 
-    global knightrect
-    knightrect = pygame.Rect(knightPosX, knight.y, KNIGHT_WIDTH, KNIGHT_HEIGHT)
+    player.knightrect = pygame.Rect(player.knightPosX, knight.y, KNIGHT_WIDTH, KNIGHT_HEIGHT)
           #print("here123")
           #print(stagePosX)
 
@@ -220,16 +236,15 @@ def handle_movement(keys_pressed, knight, playerVelocityX):
       # DOWN
 
 
-def enemyMovement(enemy):
-    global at500
+def enemyMovement(enemyKnight):
     global knight
     velocity = 2
     rightOfKnight = True
     leftOfKnight = False
 
-    if enemy.x > knight.x:
+    if enemyKnight.x > knight.x:
         rightOfKnight = True
-    elif enemy.x < knight.x:
+    elif enemyKnight.x < knight.x:
         leftOfKnight = True
 
     #totalMoved = 0
@@ -246,16 +261,12 @@ def enemyMovement(enemy):
 
 #game loop
 def main():
-    global swingTrigger
-    global blockTrigger
-    global swingCount
     run = True
     clock = pygame.time.Clock()
     while run:
         if knight.y > HEIGHT - FLOOR.height - KNIGHT_HEIGHT:
         #checking if knight is on ground
-            global JUMPING
-            JUMPING = False
+            player.jumping = False
         if knight.y < HEIGHT - FLOOR.height - KNIGHT_HEIGHT:
         #if knight is on ground dont apply gravity
             knight.y += 5
@@ -267,114 +278,94 @@ def main():
                 run = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    Swinging = True
-                    swingTrigger = True
+                    player.swinging = True
+                    player.swingTrigger = True
                     #swingCount += 1
                     #handle_swing(swingTrigger)
                     #print("swingtrigger", swingTrigger)
                 if event.button == 3:
-                    Blocking = True
-                    blockTrigger = True
+                    player.blocking = True
+                    player.blockTrigger = True
                     #handle_block(blockTrigger)
                     #print("blocktrigger", blockTrigger)
 
             else:
-                blockTrigger = False
+                player.blockTrigger = False
 
         keys_pressed = pygame.key.get_pressed()
         draw_window()
         handle_movement(keys_pressed, knight, playerVelocityX)
-        enemyMovement(enemy)
+        enemyMovement(enemyKnight)
         handle_damage()
 
     pygame.quit()
 
 def handle_damage():
-    global health
-    global enemyhealth
-    global playerPosX
-    global knightrect
-    global enemy
-    global blockTrigger
-    global enemyalive
-    global playeralive
+    global enemyKnight
 
-    if knightrect.colliderect(enemy) and not blockTrigger:
-        health -= 0.1
-    if enemy.colliderect(knightrect) and swingTrigger:
-        enemyhealth -= 1
+    if player.knightrect.colliderect(enemyKnight) and not player.blockTrigger:
+        player.health -= 0.1
+    if enemyKnight.colliderect(player.knightrect) and player.swingTrigger:
+        enemy.health -= 1
 
-    if health == 0:
+    if player.health == 0:
         print("player dead")
-        playeralive = False
-    if enemyhealth == 0:
-        enemy.x = 0
-        enemy.y = 5000
-        enemyalive = False
+        player.alive = False
+    if enemy.health == 0:
+        enemyKnight.x = 0
+        enemyKnight.y = 5000
+        enemy.alive = False
 
 
 
 
 #drawing all assets
 def draw_window():
-    global LEFT
-    global RIGHT
-    global walkCount
-    global STILL
     global x
-    global middle
-    global Swinging
-    global Blocking
-    global swingTrigger
-    global blockTrigger
-    global swingCount
-    global enemyalive
-    global playeralive
-    global enemyRange
 
-    rel_x = stagePosX % BACKGROUND_IMAGE_SCALED.get_rect().width
+    rel_x = player.stagePosX % BACKGROUND_IMAGE_SCALED.get_rect().width
     WIN.blit(BACKGROUND_IMAGE_SCALED, (rel_x - BACKGROUND_IMAGE_SCALED.get_rect().width,0))
     if rel_x < WIDTH:
          WIN.blit(BACKGROUND_IMAGE_SCALED, (rel_x, 0))
     x -= 1
 
-    if enemyalive:
-        WIN.blit(ENEMY_IMAGE_SCALED, (enemy.x, HEIGHT - FLOOR.height - ENEMY_HEIGHT))
+    if enemy.alive:
+        WIN.blit(ENEMY_IMAGE_SCALED, (enemyKnight.x, HEIGHT - FLOOR.height - ENEMY_HEIGHT))
 
-    if walkCount + 1 >= 30:
-        walkCount = 0
+    if player.walkCount + 1 >= 30:
+        player.walkCount = 0
 
-    if not(STILL):
-        if LEFT:
-            WIN.blit(walkLeft[walkCount//10], (knightPosX, knight.y))
-            walkCount += 1
-        elif RIGHT:
-            WIN.blit(walkRight[walkCount//10], (knightPosX, knight.y))
-            walkCount += 1
+    if not(player.still):
+        if player.left:
+            WIN.blit(walkLeft[player.walkCount//10], (player.knightPosX, knight.y))
+            player.walkCount += 1
+        elif player.right:
+            WIN.blit(walkRight[player.walkCount//10], (player.knightPosX, knight.y))
+            player.walkCount += 1
 
-    elif swingTrigger == True:
-        WIN.blit(swingRight[swingCount//3], (knightPosX, knight.y))
-        swingCount += 1
-        if swingCount//3 > 2:
-            swingCount = 0
-            swingTrigger = False
+    elif player.swingTrigger == True:
+        WIN.blit(swingRight[player.swingCount//3], (player.knightPosX, knight.y))
+        player.swingCount += 1
+        if player.swingCount//3 > 2:
+            player.swingCount = 0
+            player.swingTrigger = False
 
-    elif blockTrigger == True:
-        WIN.blit(KNIGHTbs1, (knightPosX, knight.y))
+    elif player.blockTrigger == True:
+        WIN.blit(KNIGHTbs1, (player.knightPosX, knight.y))
 
         #blockTrigger = False
         #print(blockTrigger, "b")
 
-    elif STILL:
-        if RIGHT:
-            WIN.blit(walkRight[0], (knightPosX, knight.y))
-        elif LEFT:
-            WIN.blit(walkLeft[0], (knightPosX, knight.y))
+    elif player.still:
+        if player.right:
+            WIN.blit(walkRight[0], (player.knightPosX, knight.y))
+        elif player.left:
+            WIN.blit(walkLeft[0], (player.knightPosX, knight.y))
 
 
 
-    if pygame.key.get_pressed()[pygame.K_SPACE] and JUMPING == False:
-        print("3")
+    if pygame.key.get_pressed()[pygame.K_SPACE] and player.jumping == False:
+        pass
 
 
     WIN.blit(FLOOR1_IMAGE_SCALED, (rel_x - FLOOR1_IMAGE_SCALED.get_rect().width,130))
@@ -397,24 +388,24 @@ def draw_window():
     myfont = pygame.font.SysFont('Comic Sans MS', 50)
     #positions = myfont.render("Playerposx: " + str(playerPosX) + " knightposx: " + str(knightPosX) + "stagePosX" + str(stagePosX), False, (0, 0, 0))
     #WIN.blit(positions,(0,100))
-    healthtext = myfont.render(str(health), False, (0, 0, 0))
+    healthtext = myfont.render(str(player.health), False, (0, 0, 0))
     #WIN.blit(healthtext, (200,20))
     pygame.font.init()
     myfont2 = pygame.font.SysFont('Comic Sans MS', 30)
-    enemyhitbox = myfont2.render("enemy.x: " + str(enemy.x) + " enemy.y: " + str(enemy.y) + " knightrect.x " + str(knightrect.x) + " knightrect.y " + str(knightrect.y), False, (0, 0, 0))
+    enemyhitbox = myfont2.render("enemy.x: " + str(enemyKnight.x) + " enemy.y: " + str(enemyKnight.y) + " knightrect.x " + str(player.knightrect.x) + " knightrect.y " + str(player.knightrect.y), False, (0, 0, 0))
     WIN.blit(enemyhitbox,(0,100))
-    healthtext = myfont.render(str(health), False, (0, 0, 0))
+    healthtext = myfont.render(str(player.health), False, (0, 0, 0))
     #WIN.blit(healthtext, (200,20))
 
     # = pygame.draw.rect(surface, color, rect)
-    player1healthbar = pygame.draw.rect(WIN, (255, 52, 25), (10, 10, health*2 , 40))
-    enemy1healthbar = pygame.draw.rect(WIN, (255, 52, 25), (WIDTH-210, 10, enemyhealth*2 , 40))
+    player1healthbar = pygame.draw.rect(WIN, (255, 52, 25), (10, 10, player.health*2 , 40))
+    enemy1healthbar = pygame.draw.rect(WIN, (255, 52, 25), (WIDTH-210, 10, enemy.health*2 , 40))
     deathtext = myfont.render("You died!", False, (255, 17, 0), )
 
     text_width = deathtext.get_width()
     text_height = deathtext.get_height()
 
-    if not playeralive:
+    if not player.alive:
         pygame.draw.rect(WIN, (0, 0, 0), (0, 0, WIDTH, HEIGHT))
         WIN.blit(deathtext, (WIDTH/2 - text_width/2, HEIGHT/2 - text_height/2))
         #print (str(text_width))
